20e4b8db431e5edd1bd984b59c7ab85b30525f17
Adom <adomfosugit@gmail.com>
2022-10-20 14:17:16 +0000

    amended

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
 from . import views
+
 urlpatterns = [
-    path('',views.home, name = 'home'),
-    path('bridalfan/', views.bridal, name = 'bridal_fan'),
-    path('#boquet/', views.boquet, name = 'boquet'),
-    path('#dowry/', views.Dowryy, name = 'dowryyy'),
-    path('#souv/', views.souv, name = 'souvens'),
+    path('', views.home, name='home'),
+
 ]
