47b2130383ffad77955e928536a10f4d06d8e017
Adom <adomfosugit@gmail.com>
2022-10-20 23:03:24 +0000

    tweaked app

--- myapp/views.py
+++ myapp/views.py
@@ -6,4 +6,4 @@ def home(request):
     my_list = Bakery.objects.all()
     return render(request,
           'events/home.html',
-            {'my_list' : my_list} )
+            {'my_list' : my_list  } )
